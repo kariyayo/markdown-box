304c6b006535682093299f5fea47a9f1950eea28
bati11 <mail.bati11@gmail.com>
2016-02-18 16:23:18 +0900

    Remove react-treeview

--- react-app/src/components/FileTree.js
+++ react-app/src/components/FileTree.js
@@ -1,41 +1,92 @@
 var React = require('react');
 var Link = require('react-router').Link;
 
-require('react-treeview/react-treeview.css');
-var TreeView = require('react-treeview');
-
 var strage = require('../strage');
+
+
+var style = {
+  rootTreeContainer: {
+    paddingLeft: "16px"
+  },
+  showChildrenTreeContainer: {
+    display: "block",
+    paddingLeft: "16px"
+  },
+  hideChildrenTreeContainer: {
+    display: "none",
+    paddingLeft: "16px"
+  },
+  node: {
+    listStyle: "none"
+  },
+  toggleButton: {
+    cursor: "pointer"
+  }
+};
 
 var TreeNode = React.createClass({
   displayName: "TreeNode",
+  getInitialState: function() {
+    return {collapse: true};
+  },
   _onClick: function() {
     this.props.onClick(this.props.entry);
+    this.setState({collapse: !this.state.collapse});
+  },
+  _showChildStyle: function() {
+    if (this.state.collapse) {
+      return style.hideChildrenTreeContainer;
+    } else {
+      return style.showChildrenTreeContainer;
+    }
+  },
+  _allow: function() {
+    if (this.state.collapse) {
+      return '[+]';
+    } else {
+      return '[-]';
+    }
   },
   render: function() {
     var entry = this.props.entry;
     var _this = this;
     if (entry.isFolder) {
       return (
-        <TreeView
-            defaultCollapsed
+        <li
             key={entry.path}
-            nodeLabel={entry.name}
-            onClick={this._onClick}>
-          {this.props.entry.children.map(function(child, i) {
-            return (
-              <TreeNode
-                  entry={child}
-                  key={i}
-                  onClick={_this.props.onClick} />
-            );
-          })}
-        </TreeView>
+            style={style.node}>
+          <div>
+            <Link to={"/viewer/preview/" + encodeURIComponent(entry.path)}>
+              {entry.name}
+            </Link>
+            {" "}
+            <span
+              onClick={this._onClick}
+              style={style.toggleButton}>
+              {this._allow()}
+            </span>
+          </div>
+          <ul style={this._showChildStyle()}>
+            {this.props.entry.children.map(function(child, i) {
+              return (
+                <TreeNode
+                    entry={child}
+                    key={i}
+                    onClick={_this.props.onClick} />
+              );
+            })}
+          </ul>
+        </li>
       );
     } else {
       return (
-        <div key={entry.path}>
-          <Link to={"/viewer/preview/" + encodeURIComponent(entry.path)}>{entry.name}</Link>
-        </div>
+        <li
+            key={entry.path}
+            style={style.node}>
+          <Link to={"/viewer/preview/" + encodeURIComponent(entry.path)}>
+            {entry.name}
+          </Link>
+        </li>
       );
     }
   }
@@ -62,7 +113,7 @@ module.exports = React.createClass({
   render: function() {
     var _this = this;
     return (
-      <div>
+      <ul style={style.rootTreeContainer}>
         {this.state.data.map(function(entry, i) {
           return (
             <TreeNode
@@ -71,7 +122,7 @@ module.exports = React.createClass({
                 onClick={_this.onNodeClick} />
           );
         })}
-      </div>
+      </ul>
     );
   }
 });

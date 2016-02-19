5e748eae911a2ac467be530fd09bb6aefd3fc9c7
bati11 <mail.bati11@gmail.com>
2016-02-19 13:10:46 +0900

    Add welcome page

---
+++ react-app/src/components/Welcome.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var Grid = require('react-bootstrap').Grid;
+var Row = require('react-bootstrap').Row;
+var Col = require('react-bootstrap').Col;
+var Jumbotron = require('react-bootstrap').Jumbotron;
+var Button = require('react-bootstrap').Button;
+var LinkContainer = require('react-router-bootstrap').LinkContainer;
+
+module.exports = React.createClass({
+  displayName: "Welcome",
+  render: function() {
+    return (
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={12} md={12}>
+            <Jumbotron>
+              <h1>MD-Box</h1>
+              <p>{'MD-Box will in a little happy to your markdown in Dropbox'}</p>
+              <p>
+                <LinkContainer to={'/viewer'}>
+                  <Button bsStyle="primary">{'Try out'}</Button>
+                </LinkContainer>
+              </p>
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+});
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,19 +3,22 @@ var ReactDOM = require('react-dom');
 
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
+var Redirect = require('react-router').Redirect;
 var IndexRedirect = require('react-router').IndexRedirect;
 var browserHistory =require('react-router').browserHistory;
 
 require('bootstrap/dist/css/bootstrap.css');
 require('bootstrap');
 
+var Welcome = require('./components/Welcome');
 var App = require('./components/App');
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route name="TOP" path="/" component={App} />
+    <Route path="welcome" component={Welcome} />
     <Route path="viewer" component={App} />
     <Route path="viewer/:path" component={App} />
+    <Redirect from="/" to="welcome" />
   </Router>,
   document.getElementById('root')
 );

394844fdc43d1705561ce3a1d3396ab1f0675028
bati11 <mail.bati11@gmail.com>
2017-04-09 15:34:22 +0900

    started implementing a vue js app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <my-header />
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-app/src/components/MyHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <navbar placement="top" type="default">
+      <a slot="brand" href="/" title="Markdown-box" class="navbar-brand">Markdown-box</a>
+    </navbar>
+  </div>
+</template>
+
+<script>
+import { navbar } from 'vue-strap'
+
+export default {
+  name: 'my-header',
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style scoped>
+</style>

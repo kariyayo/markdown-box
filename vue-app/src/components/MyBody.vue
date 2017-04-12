8a1dd349feaee44c2b18941de87e856923dfb5a3
bati11 <mail.bati11@gmail.com>
2017-04-12 21:58:20 +0900

    add MyContent component

--- vue-app/src/components/MyBody.vue
+++ vue-app/src/components/MyBody.vue
@@ -2,14 +2,21 @@
   <div>
     <el-row>
       <el-col :span="4">left</el-col>
-      <el-col :span="20">content</el-col>
+      <el-col :span="20">
+        <my-content></my-content>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import MyContent from './MyContent.vue'
+
 export default {
-  name: 'my-body'
+  name: 'my-body',
+  components: {
+    MyContent
+  }
 }
 </script>
 
---
+++ vue-app/src/components/MyContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    hogehoge
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-content'
+}
+</script>
+
+<style scoped>
+</style>

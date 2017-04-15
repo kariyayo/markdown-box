4f3121a5f20cd5b89e104da5a15cffe3d601bbcb
bati11 <mail.bati11@gmail.com>
2017-04-15 19:33:17 +0900

    add a table

--- vue-app/src/components/MyContent.vue
+++ vue-app/src/components/MyContent.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    hogehoge
+  <div id="my-content">
+    <el-table :data="tableData" :show-header="false" class="content-table" >
+      <el-table-column prop="fileName"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'my-content'
+  name: 'my-content',
+  data () {
+    return {
+      tableData: [
+        { fileName: 'hogehoge' },
+        { fileName: 'fugafuga' }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.content-table {
+  text-align: left;
+}
 </style>
